90960139db64582172babd3f2cb61ce2d55b0270
euadi73 <niculaieadrian20@stud.ase.ro>
2024-10-06 19:39:34 +0300

    Sem1 DAM

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Seminar1_dam_Niculaie Adrian-Alexandru"
+include(":app")
+include(":app:seminar01_modul")
